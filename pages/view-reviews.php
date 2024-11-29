0bbdc85ae885dde8de4555535a218a8d97de0b11
Fadeyboi <fadealone44@gmail.com>
2024-11-29 19:56:27 +0300

    tt

--- pages/view-reviews.php
+++ pages/view-reviews.php
@@ -1,4 +1,6 @@
 <?php
+include '../includes/header.php';
+
 $servername = "mysql.railway.internal";
 $username = "root";
 $password = "sFIdChKeMCCdhWvpFEUfWMjAlzoDAgkX";
@@ -9,7 +11,7 @@ if ($conn->connect_error) {
     die("Error connecting to the database.");
 }
 
-$sql = "SELECT r.id, r.name, r.email, h.name AS hero_name, r.rating, r.recommend, r.server, r.feedback
+$sql = "SELECT r.id, r.name, r.email, h.name AS hero_name, r.rating, r.recommend, r.server, r.feedback, r.difficult
         FROM reviews r
         LEFT JOIN heroes h ON r.hero_id = h.id
         ORDER BY r.id DESC";
@@ -23,16 +25,10 @@ $result = $stmt->get_result();
 
 <head>
     <title>View Reviews</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="icon" type="image/png" href="../images/website-logo.png" />
-    <link rel="stylesheet" href="../global/styles.css" />
 </head>
 
 <body>
-    <?php include '../includes/header.php'; ?>
-    <div id="main">
+    <main>
         <h1>User Reviews</h1>
         <?php if ($result->num_rows > 0): ?>
             <table>
@@ -44,6 +40,7 @@ $result = $stmt->get_result();
                         <th>Favorite Hero</th>
                         <th>Rating</th>
                         <th>Recommend</th>
+                        <th>Difficult</th>
                         <th>Server</th>
                         <th>Feedback</th>
                     </tr>
@@ -57,6 +54,7 @@ $result = $stmt->get_result();
                             <td><?php echo htmlspecialchars($row['hero_name']); ?></td>
                             <td><?php echo htmlspecialchars($row['rating']); ?></td>
                             <td><?php echo htmlspecialchars($row['recommend']); ?></td>
+                            <td><?php echo htmlspecialchars($row['difficult']); ?></td>
                             <td><?php echo htmlspecialchars($row['server']); ?></td>
                             <td><?php echo htmlspecialchars($row['feedback']); ?></td>
                         </tr>
@@ -66,10 +64,12 @@ $result = $stmt->get_result();
         <?php else: ?>
             <p>No reviews found.</p>
         <?php endif; ?>
-    </div>
+    </main>
     <?php include '../includes/footer.php'; ?>
 </body>
 
 </html>
-<?php $stmt->close();
-$conn->close(); ?>
+<?php
+$stmt->close();
+$conn->close();
+?>
