58b4f5995216e41c0a0bea03a7f4e2ece4a6168e
Fadeyboi <fadealone44@gmail.com>
2024-11-29 18:43:20 +0300

    t

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 <head>
   <title>Deadlock Wiki</title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <link rel="icon" type="image/png" href="./images/website-logo.png" />
+  <link rel="stylesheet" href="./global/styles.css" />
 </head>
 <?php include './includes/indexHeader.php'; ?>
 
