37baebb5af85cfb00086545061d81381448845e9
Fadeyboi <fadealone44@gmail.com>
2024-11-29 19:37:19 +0300

    fff

--- includes/footer.php
+++ includes/footer.php
@@ -14,19 +14,20 @@
     </a>
     <div style="display: flex; gap: 1em;">
       <!-- W3C Markup Validation Image -->
-      <a href="https://validator.w3.org/check?uri=referer" target="_blank">
+      <a href="https://validator.w3.org/check?uri=<?php echo urlencode('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>">
         <img
           src="https://www.w3.org/Icons/valid-xhtml10"
           alt="Valid XHTML 1.0 Strict"
           style="width: 88px; height: 31px;" />
       </a>
       <!-- W3C CSS Validation Image -->
-      <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+      <a href="https://jigsaw.w3.org/css-validator/validator?uri=<?php echo urlencode('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>">
         <img
           src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
           alt="Valid CSS"
           style="width: 88px; height: 31px;" />
       </a>
     </div>
+
   </div>
 </div>
