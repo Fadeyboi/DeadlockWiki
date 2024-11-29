8c1c7839a2a11c486e0db2dc296d40de08e27755
Fadeyboi <fadealone44@gmail.com>
2024-11-29 19:49:36 +0300

    dd

--- pages/heroes-bebop.php
+++ pages/heroes-bebop.php
@@ -80,10 +80,12 @@ if ($result->num_rows > 0) {
     </div>
     <div>
       <h2>Basic Combo</h2>
-      <video width="560" height="315" controls>
-        <source src="<?php echo htmlspecialchars($hero['combo_video']); ?>" type="video/mp4" />
-        <p>Your browser does not support the video tag. You can <a href="<?php echo htmlspecialchars($hero['combo_video']); ?>">download the video</a> instead.</p>
-      </video>
+      <object width="560" height="315" data="<?php echo htmlspecialchars($hero['combo_video']); ?>" type="video/mp4">
+        <p>Your browser does not support embedded videos. You can
+          <a href="<?php echo htmlspecialchars($hero['combo_video']); ?>">download the video</a> instead.
+        </p>
+      </object>
+
     </div>
     <div>
       <h2>Lore</h2>
