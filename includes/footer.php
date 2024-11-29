376ea88a6e9312f1aa43b269695764ed2d3d58fb
Fadeyboi <fadealone44@gmail.com>
2024-11-29 16:11:47 +0300

    f

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,11 @@
 <?php
 if (!defined('BASE_URL')) {
-  define('BASE_URL', '/DeadlockWiki');
+  $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+  $host = $_SERVER['HTTP_HOST'];
+  $path = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+  define('BASE_URL', $protocol . '://' . $host . $path);
 }
+
 ?>
 
 <footer>
